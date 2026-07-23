6d83bb1b54526ea67dffeef855f6f935e016ad10
ohjeongsuk <ojs933327@gmail.com>
2026-07-23 12:48:21 +0900

    [리액트 자바스크립트]콜백함수

---
+++ section01/chapter07.js
@@ -0,0 +1,39 @@
+// 함수선언문(호이스팅)
+function checkMood(mood, goodCallback, badCallback) {
+  if (mood === "good") {
+    // sing()
+    // dance()
+    goodCallback()
+  } else {
+    // cry()
+    badCallback()
+  }
+}
+
+checkMood("good", ()=> console.log("노래 부르면서 춤을 춥니다."), ()=> console.log("기분이 안좋다."));
+
+/*
+// 함수선언문
+function sing() {
+  console.log("노래 부르기.")
+}
+
+let sing = ()=> console.log("노래 부르기.")
+
+function cry() {
+  console.log("노래를 부르지 못하여 울고있다.")
+}
+
+function dance() {
+  console.log("춤을 추고 있다.")
+}
+*/
+
+//2. 콜백함수 응용방법
+function repeat(count, callback) {
+  for (let index = 0; index < count; index++) {
+    callback(index);
+  }
+}
+
+repeat(5, (idx)=> console.log("ㅎㅇㅌ"+idx))
